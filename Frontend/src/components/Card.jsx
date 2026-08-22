884cd77de0e16fd94590d84cad931992356253d4
Ayush Shrivastava <shrivastava.ayush0007@gmail.com>
2026-08-22 14:16:30 +0530

    test ui changes for small devices

--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -14,7 +14,7 @@ export default function Card({ data, reference, onUpdate }) {
 
     return (
         <>
-            <motion.div drag dragConstraints={reference} whileDrag={{ cursor: "grabbing", scale: 1.1 }} className="shrink-0 overflow-hidden relative rounded-[14px] w-60 h-52 bg-zinc-900/90 text-white px-7 py-10">
+            <motion.div drag dragConstraints={reference} whileDrag={{ cursor: "grabbing", scale: 1.1 }} className="md:h-52 shrink-0 overflow-hidden relative rounded-[14px] w-60 h-36 bg-zinc-900/90 text-white md:px-7 md:py-10 px-3 py-6">
                 <IoDocumentTextOutline />
 
                 <p 
@@ -27,7 +27,7 @@ export default function Card({ data, reference, onUpdate }) {
                         <h5 className='text-xs'>{data.filesize}</h5>
                         <span onClick={openModal} className="w-6 h-6 rounded-full flex items-center justify-center bg-zinc-700 cursor-pointer"><FaDownload size={12} /></span>
                     </div>
-                    <div className={`tag w-full py-3 bg-${data.tag.tagColor}-500 flex items-center justify-center`}>
+                    <div className={` hidden md:flex tag w-full py-3 bg-${data.tag.tagColor}-500 flex items-center justify-center`}>
                         <h3 className="text-sm font-semibold">{data.tag.tagTitle}</h3>
                     </div>
                 </div>
